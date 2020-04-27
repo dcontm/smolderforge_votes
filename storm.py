84691b121b2a647ecd32a0564538c618dc96dc44
dcontm <dcontm@gmail.com>
2020-04-28 01:14:51 +0300

    proxy check fix

--- storm.py
+++ storm.py
@@ -24,16 +24,10 @@ class Storm:
 		for row in proxy_table.tbody.find_all('tr'):
 
 			#выбираем из каждой строки таблицы столбцы с ip и портом
-			#проверяем на доступность и доступные добавляем в список который вернет функция
 			proxy = row.find_all('td')[0].string+':'+row.find_all('td')[1].string
-			proxies = {'http': proxy}	# аргумент для запроса доступности
-			response=requests.get(URL, proxies=proxies, timeout=3)
+			proxies_list.append(proxy)
 
-			if response.status_code == 200:
-				proxies_list.append(proxy)
-
-		print('Провера прокси завершена, подготовлено {} адресов'.format(len(proxies_list)))
-		print(proxies_list)
+		print('Подготовлено {} адресов'.format(len(proxies_list)))
 
 		return proxies_list
 
@@ -58,46 +52,55 @@ class Storm:
 
 		for every in acc:
 			# для каждого аккаунта формируем запрос с новым прокси
-			proxy = proxies_list.pop()
-			print(proxy)
+			for i in proxies_list:
+				# перебираем провкси до тех пор пока запрос не пройдет
+				proxy = proxies_list.pop()
+				print(proxy)
 
-			prox = Proxy()
-			prox.proxy_type = ProxyType.MANUAL
-			prox.http_proxy = proxy
-			prox.ssl_proxy = proxy
-			capabilities = webdriver.DesiredCapabilities.CHROME
-			prox.add_to_capabilities(capabilities)
+				prox = Proxy()
+				prox.proxy_type = ProxyType.MANUAL
+				prox.http_proxy = proxy
+				prox.ssl_proxy = proxy
+				capabilities = webdriver.DesiredCapabilities.CHROME
+				prox.add_to_capabilities(capabilities)
 
-			if headless == True:
-				# режим запуска вебдрайвера по умолчанию безголовый
-				chrome_options = webdriver.ChromeOptions()
-				chrome_options.add_argument("--headless")
-				driver = webdriver.Chrome(executable_path='/home/dcontm/Рабочий стол/develop/storm/venv/chromedriver', desired_capabilities=capabilities,chrome_options=chrome_options)
+				if headless == True:
+					# режим запуска вебдрайвера по умолчанию безголовый
+					chrome_options = webdriver.ChromeOptions()
+					chrome_options.add_argument("--headless")
+					driver = webdriver.Chrome(executable_path='/home/dcontm/Рабочий стол/develop/storm/venv/chromedriver', desired_capabilities=capabilities,chrome_options=chrome_options)
 
-			else:
-				driver = webdriver.Chrome(executable_path='/home/dcontm/Рабочий стол/develop/storm/venv/chromedriver', desired_capabilities=capabilities)
+				else:
+					driver = webdriver.Chrome(executable_path='/home/dcontm/Рабочий стол/develop/storm/venv/chromedriver', desired_capabilities=capabilities)
+
+				try:
+					driver.get(URL+'vote')
+					time.sleep(10)
+
+					username = driver.find_element_by_id('login_userinput')
+					password = driver.find_element_by_id('login_passinput')
+					login = driver.find_element_by_id('login_submit_1')
 
 
-			driver.get(URL+'vote')
-			time.sleep(6)
+					username.send_keys(every[0])
+					password.send_keys(every[1])
+					login.click() # аутентифицируемся
+					print("Вход в систему выполнен.")
+					print(every[0])
+					time.sleep(5)
 
-			username = driver.find_element_by_id('login_userinput')
-			password = driver.find_element_by_id('login_passinput')
-			login = driver.find_element_by_id('login_submit_1')
+					votes = driver.find_elements_by_class_name('vote_link_button')
 
+					for vote in votes:
+					# поочередно сликаем на кнопку голосования
+						vote.click()
+						print("Голосование успешною.")
+						time.sleep(3)
 
-			username.send_keys(every[0])
-			password.send_keys(every[1])
-			login.click() # аутентифицируемся
-			print("Вход в систему выполнен.")
-			time.sleep(3)
+					break # если голосование успешно произведено прерываем цикл и переходим к следующему аккаунту
+				except:
+					continue 
 
-			votes = driver.find_elements_by_class_name('vote_link_button')
-
-			for vote in votes:
-				# поочередно сликаем на кнопку голосования
-				vote.click()
-				print("Голосование успешною.")
-				time.sleep(3)
-
-			driver.quit()
+				driver.quit()
+s=Storm()
+s.run()
